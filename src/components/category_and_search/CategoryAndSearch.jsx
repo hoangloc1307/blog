51793e894ac7b5fc9808933e70228b1fae582597
Lộc Trần <hoangloc1307@gmail.com>
2022-04-14 21:03:21 +0700

    Repush

--- src/components/category_and_search/CategoryAndSearch.jsx
+++ src/components/category_and_search/CategoryAndSearch.jsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import React, { memo, useContext } from "react";
-import { Link } from "react-router-dom";
 import { CategoryContext } from "../../App";
 import c from "./CategoryAndSearch.module.scss";
 
